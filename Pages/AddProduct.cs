324e84d172917ad1d8f13aa637189a43f7f4a8a0
aymenestaff <126107820+aymenestaff@users.noreply.github.com>
2024-11-25 16:56:06 -0500

    add product page

---
+++ Pages/AddProduct.cs
@@ -0,0 +1,33 @@
+using EFTuto.Models;
+using EFTuto.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class AddProductModel : PageModel
+{
+    [BindProperty]
+    public Product Product { get; set; } = new Product();
+
+    private readonly ILogger<AddProductModel> _logger;
+    private readonly IProductService _productService;
+
+    public AddProductModel(ILogger<AddProductModel> logger, IProductService productService)
+    {
+        _logger = logger;
+        _productService = productService;
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+
+            return Page();
+        }
+
+        _productService.AddProduct(Product);
+
+        TempData["Message"] = "Product added successfully!";
+        return RedirectToPage("Product"); // Redirect to a product list page
+    }
+}
--- Pages/Product.cs
+++ Pages/Product.cs
@@ -1,3 +1,4 @@
+using EFTuto.Models;
 using EFTuto.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
